383f79369aa08a4347228cef39e95c6fe8a8e73a
Arenz <if.arenz@gmail.com>
2024-02-13 11:41:21 +0100

    first page started

--- EasySaveWPF/MainWindow.xaml.cs
+++ EasySaveWPF/MainWindow.xaml.cs
@@ -1,24 +1,37 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EasySaveWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuPrincipal_Click(object sender, RoutedEventArgs e)
+        {
+            // Exemple de chargement du menu principal dans le Frame
+            ContentFrame.Navigate(new Uri("Views/MenuPrincipal.xaml", UriKind.Relative));
+        }
+
+        private void Vue1_Click(object sender, RoutedEventArgs e)
+        {
+            // Charger la Vue 1 dans le Frame
+            ContentFrame.Navigate(new Uri("Views/Vue1.xaml", UriKind.Relative));
+        }
+
+        private void Vue2_Click(object sender, RoutedEventArgs e)
+        {
+            // Charger la Vue 2 dans le Frame
+            ContentFrame.Navigate(new Uri("Views/Vue2.xaml", UriKind.Relative));
+        }
+
+        private void LanguageSettingsPage(object sender, RoutedEventArgs e)
+        {
+            // Charger la Vue 2 dans le Frame
+            ContentFrame.Navigate(new Uri("Views/LanguageSettingsView.xaml", UriKind.Relative));
         }
     }
 }
---
+++ EasySaveWPF/Views/LanguageSettingsView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EasySaveWPF.Views
+{
+    public partial class LanguageSettingsPage : Page
+    {
+        public LanguageSettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedLanguage = ((ComboBoxItem)LanguageComboBox.SelectedItem).Content.ToString();
+            switch (selectedLanguage)
+            {
+                case "Français":
+                    ChangeCulture("fr-FR");
+                    break;
+                case "English":
+                    ChangeCulture("en-US");
+                    break;
+                    // Ajouter d'autres cas pour d'autres langues
+            }
+            MessageBox.Show("La langue a été changée. Veuillez redémarrer l'application pour appliquer les changements.", "Changement de langue", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ChangeCulture(string cultureCode)
+        {
+            CultureInfo culture = new CultureInfo(cultureCode);
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+            // Vous devrez ajouter la logique pour actualiser les vues si nécessaire
+        }
+    }
+}
