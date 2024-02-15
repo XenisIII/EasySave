60417a3b9ff588b579dc6a61518cb940b2704644
Arenz <if.arenz@gmail.com>
2024-02-15 09:51:40 +0100

    added log selection ui

--- EasySave/obj/Debug/net8.0/EasySave.AssemblyInfo.cs
+++ EasySave/obj/Debug/net8.0/EasySave.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc64e0a4933f940f6a1003220551b2fa26b6321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06cfd8556ee98a451cc4b5b48704f1adb50168ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
