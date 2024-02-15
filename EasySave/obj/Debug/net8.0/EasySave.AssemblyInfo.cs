29ae68766e624d3d44d4d754c06b1aa3061b564f
Arenz <if.arenz@gmail.com>
2024-02-15 09:26:44 +0100

    corrected duplicate files

--- EasySave/obj/Debug/net8.0/EasySave.AssemblyInfo.cs
+++ EasySave/obj/Debug/net8.0/EasySave.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79cdc4496be8cab3fe73cd266941f1c3511ed07e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfcd5d4ac935ed04f7f10d337091b8c13ecedceb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
